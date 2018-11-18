15546bb22d54ed6586da80e201e98c9b85cebe2c
Igor <igor_pavlenko@yahoo.com>
2018-11-18 11:32:32 +0000

    dynamic ingr output

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngridients/BurgerIngredient';
 
 const burger = (props) => {
+    
+    
+    const transformedIngredients = Object.keys(props.ingredients).map(igridKey => {
+       return [...Array(props.ingredients[igridKey])].map((_, i) => {
+          return <BurgerIngredient key={igridKey + i} type={igridKey} />;
+       });
+    });
+    
+    console.log(transformedIngredients);
+    
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
