beb66effcae0a3689b0ebf498f04a4dd7eae2ba9
Igor <igor_pavlenko@yahoo.com>
2018-11-18 12:45:28 +0000

    save before the build control implementation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,20 @@ import BurgerIngredient from './BurgerIngridients/BurgerIngredient';
 const burger = (props) => {
     
     
-    const transformedIngredients = Object.keys(props.ingredients).map(igridKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igridKey => {
        return [...Array(props.ingredients[igridKey])].map((_, i) => {
           return <BurgerIngredient key={igridKey + i} type={igridKey} />;
        });
-    });
+    })
+    .reduce((arr, el) => { // pass here prev value & current value
+        return arr.concat(el); // here we add them both 
+    }, );
     
     console.log(transformedIngredients);
+    
+    if(transformedIngredients.length === 0){
+       transformedIngredients = <p>Please Start Adding Some Ingredients</p>;
+    }
     
     return(
         <div className={classes.Burger}>
