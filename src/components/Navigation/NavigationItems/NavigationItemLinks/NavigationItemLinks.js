eb673aa0a31b76e11e1b483570584d36fa5382e6
Igor <igor_pavlenko@yahoo.com>
2018-11-22 11:32:17 +0000

    mobile deveice side drawer styles

--- src/components/Navigation/NavigationItems/NavigationItemLinks/NavigationItemLinks.js
+++ src/components/Navigation/NavigationItems/NavigationItemLinks/NavigationItemLinks.js
@@ -2,13 +2,13 @@ import React from 'react';
 import classes from './NavigationItemLinks.css';
 
 const navigationItemLinks = (props) => (
-            
+
         <li className=
             {classes.NavigationItemsLinks}>
             <a href={props.link} 
             className={props.active ? classes.active : null}>{props.children}</a>
         </li>
-        
-    );
-    
+
+);
+
 export default navigationItemLinks;
