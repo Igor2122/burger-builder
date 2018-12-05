95dd5488d85eacb88101221be363b777d46d1dc5
Igor <igor_pavlenko@yahoo.com>
2018-12-05 11:14:05 +0300

    input element added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -46,13 +47,16 @@ class ContactData extends Component {
     
     render () {
         
-        let form = (<form>
-                        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                        <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                        <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+        let form = (
+                    <form>
+                        <Input inputtype="input" label="Label" type="text" name="name" placeholder="Enter Your Name" />
+                        <Input inputtype="input" label="Label" type="email" name="email" placeholder="Your Email" />
+                        <Input inputtype="input" label="Label" type="text" name="street" placeholder="Street" />
+                        <Input inputtype="input" label="Label" type="text" name="postal" placeholder="Postal Code" />
+
                         <Button btntype="Success" clicked={this.orderHandler}>ORDER</Button>
-                    </form>);
+                    </form>
+                    );
         if (this.state.loading) {
             form = <Spinner />;
         }
