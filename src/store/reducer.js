40a7862ab8b149fa7e2fed8aa1c103cba8f2af7d
Igor <igor_pavlenko@yahoo.com>
2018-12-13 13:18:19 +0000

    connected store to reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    salad: 0.5,
+    cheese: 0.4,
+};
+
+const reduser = (state = initialState, action) => {
+    return state;
+};
+
+export default reduser;
