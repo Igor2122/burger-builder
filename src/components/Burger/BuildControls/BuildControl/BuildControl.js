ecc94e9f7e6029d499b32b5bd9bb69cd466c2cbf
Igor <igor_pavlenko@yahoo.com>
2018-12-17 11:52:42 +0300

    updated the labesls show

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ import classes from './BuildControl.css';
 const buildControl = (props) => {
     return (
         <div className={classes.BuildControl}> 
-            <div className={classes.Label}>{props.Label}</div>
+            <div className={classes.Label}>{props.label}</div>
             <button 
                 className={classes.Less}
                 onClick={props.removed}
